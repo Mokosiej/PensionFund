b060f552c2c57a6e12c0d64e12037a43736a4cd7
Tatsiana <mokosiejtatsiana@gmail.com>
2024-05-22 19:03:24 +0200

    first commit

--- src/PensionFund.java
+++ src/PensionFund.java
@@ -1,59 +1,59 @@
 public class PensionFund {
     private static final double PENSION_RATE = 0.7;
 
-        private String fundName;
-        private int foundationYear;
-        private int numberOfContributors;
-        private double totalAssets;
-        private String address;
+    private String fundName;
+    private int foundationYear;
+    private int numberOfContributors;
+    private double totalAssets;
+    private String address;
 
-        public PensionFund(String fundName, int foundationYear, int numberOfContributors, double totalAssets, String address) {
-            this.fundName = fundName;
-            this.foundationYear = foundationYear;
-            this.numberOfContributors = numberOfContributors;
-            this.totalAssets = totalAssets;
-            this.address = address;
-        }
-
-        @Override
-        public String toString() {
-            return "PensionFund{" +
-                    "fundName='" + fundName + '\'' +
-                    ", foundationYear=" + foundationYear +
-                    ", numberOfContributors=" + numberOfContributors +
-                    '}';
-        }
-
-
-        public String getFundName() {
-            return fundName;
-        }
-
-        public void setFundName(String fundName) {
-            this.fundName = fundName;
-        }
-
-        public int getFoundationYear() {
-            return foundationYear;
-        }
-
-        public void setFoundationYear(int foundationYear) {
-            this.foundationYear = foundationYear;
-        }
-
-        public int getNumberOfContributors() {
-            return numberOfContributors;
-        }
-
-        public void setNumberOfContributors(int numberOfContributors) {
-            this.numberOfContributors = numberOfContributors;
-        }
-
-
-        public double calculateFuturePension(Person person) {
-            return 0.7 * person.calculateAverageSalary();
-        }
-
-
-
+    public PensionFund(String fundName, int foundationYear, int numberOfContributors, double totalAssets, String address) {
+        this.fundName = fundName;
+        this.foundationYear = foundationYear;
+        this.numberOfContributors = numberOfContributors;
+        this.totalAssets = totalAssets;
+        this.address = address;
     }
+
+    @Override
+    public String toString() {
+        return "PensionFund{" +
+                "fundName='" + fundName + '\'' +
+                ", foundationYear=" + foundationYear +
+                ", numberOfContributors=" + numberOfContributors +
+                '}';
+    }
+
+
+    public String getFundName() {
+        return fundName;
+    }
+
+    public void setFundName(String fundName) {
+        this.fundName = fundName;
+    }
+
+    public int getFoundationYear() {
+        return foundationYear;
+    }
+
+    public void setFoundationYear(int foundationYear) {
+        this.foundationYear = foundationYear;
+    }
+
+    public int getNumberOfContributors() {
+        return numberOfContributors;
+    }
+
+    public void setNumberOfContributors(int numberOfContributors) {
+        this.numberOfContributors = numberOfContributors;
+    }
+
+
+    public double calculatePension(Person person) {
+        double averageSalary = person.calculateAverageSalary();
+        return averageSalary * PENSION_RATE;
+    }
+
+
+}
